7ed812f900c36889aaee693c54c2f862cdeca8bc
Josh Cummings <josh.cummings@gmail.com>
2020-05-08 15:59:56 -0600

    Step Six: Database Users
    Now that we've got some users in a database, we can use
    JdbcUserDetailsManager, which uses JDBC to query for users.
    
    Since we haven't added user authorities just yet, we're hard-coding the
    list of authorities for each user to be resolution:read.

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -3,20 +3,25 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 @SpringBootApplication
 public class ResolutionsApplication {
 
 	@Bean
-	public UserDetailsService userDetailsService() {
-		return new InMemoryUserDetailsManager(
-				org.springframework.security.core.userdetails.User
-						.withUsername("user")
-						.password("{bcrypt}$2a$10$3njzOWhsz20aimcpMamJhOnX9Pb4Nk3toq8OO0swIy5EPZnb1YyGe")
-						.authorities("resolution:read")
-						.build());
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource) {
+			@Override
+			protected List<GrantedAuthority> loadUserAuthorities(String username) {
+				return AuthorityUtils.createAuthorityList("resolution:read");
+			}
+		};
 	}
 
 	public static void main(String[] args) {
