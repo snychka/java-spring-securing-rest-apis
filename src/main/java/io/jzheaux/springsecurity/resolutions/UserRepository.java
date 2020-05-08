eb77cced4a17a3faa559cf47445cc9b1a57fc374
Josh Cummings <josh.cummings@gmail.com>
2020-05-08 15:50:45 -0600

    Step Four: A Spring Data Repository
    We can use a Spring Data repository to manage our users, so in this step
    we simply add one, which we'll use in the next step.

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepository.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepository.java
@@ -1,4 +1,10 @@
 package io.jzheaux.springsecurity.resolutions;
 
-public interface UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, UUID> {
 }
