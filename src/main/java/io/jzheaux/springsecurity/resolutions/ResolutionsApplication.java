0937e3b4dcee195a14a577888215a25204f04fd7
Josh Cummings <josh.cummings@gmail.com>
2020-05-08 15:34:26 -0600

    Step One: Enable Auto-configuration
    By default, Spring Boot with auto-configure Spring Security to protect
    all endpoints. It will also create a user with a generated password.
    
    So that you could better envision life without the auto-configuration,
    this was manually shut off in the project.
    
    In a real project, all that's necessary to get this behavior is to add
    the spring-boot-starter-security dependency.

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -2,9 +2,8 @@ package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class ResolutionsApplication {
 
 	public static void main(String[] args) {
