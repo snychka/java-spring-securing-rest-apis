59e6b3a83dc2a2a904555548d5c5132e97f13198
Josh Cummings <josh.cummings@gmail.com>
2020-05-08 15:48:08 -0600

    Step Three: A User Entity
    To manage users with Spring Data JPA, we'll need a JPA-managed entity.
    By default, Spring Security expects the tables and columns to be named
    something in particular.

--- src/main/java/io/jzheaux/springsecurity/resolutions/User.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/User.java
@@ -1,6 +1,62 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.UUID;
 
+@Entity(name="users")
 public class User implements Serializable {
+	@Id
+	UUID id;
+
+	@Column(name="username")
+	String username;
+
+	@Column
+	String password;
+
+	@Column
+	boolean enabled = true;
+
+	User() {}
+
+	User(String username, String password) {
+		this.id = UUID.randomUUID();
+		this.username = username;
+		this.password = password;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
 }
