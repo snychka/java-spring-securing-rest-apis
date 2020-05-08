272d49cae8eeef2937f8ac81e304488e4162e905
Josh Cummings <josh.cummings@gmail.com>
2020-05-08 16:55:29 -0600

    Step Nine: Filter-based Authorization
    We can specify which endpoints require resolution:read or
    resolution:write permission using the Spring Security DSL. In a
    future module, we'll take a look at the several ways we can also do
    this with method security.

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -3,15 +3,26 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
 
+import static org.springframework.http.HttpMethod.GET;
+
 @SpringBootApplication
-public class ResolutionsApplication {
+public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests(authz -> authz
+				.mvcMatchers(GET, "/resolutions", "/resolution/**").hasAuthority("resolution:read")
+				.anyRequest().hasAuthority("resolution:write"))
+			.httpBasic(basic -> {});
+	}
 
 	@Bean
 	public UserDetailsService userDetailsService(DataSource dataSource) {
