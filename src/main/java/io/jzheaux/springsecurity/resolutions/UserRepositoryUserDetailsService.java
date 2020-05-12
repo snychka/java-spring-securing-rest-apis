ea736c6856d25c1b289971ff8074fb43e78999e9
Josh Cummings <josh.cummings@gmail.com>
2020-05-12 10:24:54 -0600

    Step Eleven: A Minimal UserDetailsService
    A UserDetailsService should throw a UserNotFoundException if it can't
    find a user. For that reason, a simple implementation is one that always
    throws this exception.

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -1,4 +1,12 @@
 package io.jzheaux.springsecurity.resolutions;
 
-public class UserRepositoryUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		throw new UsernameNotFoundException("no user");
+	}
 }
