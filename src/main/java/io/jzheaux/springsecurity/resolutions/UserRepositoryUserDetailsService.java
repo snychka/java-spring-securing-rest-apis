c6735fa8918d0402eb01d2b263c8ddd336fc41cc
Josh Cummings <josh.cummings@gmail.com>
2020-05-12 10:29:56 -0600

    Step Thirteen: Converting a Custom Domain to UserDetails
    The preferred way to convert from a custom domain object to UserDetails
    is to use a private static class inside your custom UserDetailsService.
    The reason for that is so that your domain object doesn't have any
    dependencies on Spring Security.

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -1,8 +1,13 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class UserRepositoryUserDetailsService implements UserDetailsService {
 	private final UserRepository users;
@@ -13,6 +18,36 @@ public class UserRepositoryUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		throw new UsernameNotFoundException("no user");
+		return this.users.findByUsername(username)
+				.map(BridgedUser::new)
+				.orElseThrow(() -> new UsernameNotFoundException("no user"));
+	}
+
+	private static class BridgedUser extends User implements UserDetails {
+		public BridgedUser(User user) {
+			super(user);
+		}
+
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			return this.userAuthorities.stream()
+					.map(a -> new SimpleGrantedAuthority(a.authority))
+					.collect(Collectors.toList());
+		}
+
+		@Override
+		public boolean isAccountNonExpired() {
+			return this.enabled;
+		}
+
+		@Override
+		public boolean isAccountNonLocked() {
+			return this.enabled;
+		}
+
+		@Override
+		public boolean isCredentialsNonExpired() {
+			return this.enabled;
+		}
 	}
 }
