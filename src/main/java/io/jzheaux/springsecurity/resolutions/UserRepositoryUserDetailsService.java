6b9c78f6e91340cedce7889326a1fd3fb35081c9
Josh Cummings <josh.cummings@gmail.com>
2020-05-12 10:26:51 -0600

    Step Twelve: A Spring Data-based UserDetailsService
    A UserDetailsService is designed to be able to query any repository by
    username, and Spring Data repositories are a great fit.

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -5,6 +5,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserRepositoryUserDetailsService implements UserDetailsService {
+	private final UserRepository users;
+
+	public UserRepositoryUserDetailsService(UserRepository users) {
+		this.users = users;
+	}
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		throw new UsernameNotFoundException("no user");
