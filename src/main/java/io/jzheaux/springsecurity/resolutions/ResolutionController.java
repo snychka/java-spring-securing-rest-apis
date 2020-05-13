10641ce07a6262db5c4015c0bbc2a05d0f7453c1
Josh Cummings <josh.cummings@gmail.com>
2020-05-13 11:02:32 -0600

    Step Two: Authorize before a method is invoked
    In Spring Security, you can use filter-based authorization, as seen in
    the previous module, or you can use method-based. The @PreAuthorize
    annotation describes what authorities are needed in the same way that
    the hasAuthority method does in the Spring Security DSL.

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -1,5 +1,6 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,22 +20,26 @@ public class ResolutionController {
 		this.resolutions = resolutions;
 	}
 
+	@PreAuthorize("hasAuthority('resolution:read')")
 	@GetMapping("/resolutions")
 	public Iterable<Resolution> read() {
 		return this.resolutions.findAll();
 	}
 
+	@PreAuthorize("hasAuthority('resolution:read')")
 	@GetMapping("/resolution/{id}")
 	public Optional<Resolution> read(@PathVariable("id") UUID id) {
 		return this.resolutions.findById(id);
 	}
 
+	@PreAuthorize("hasAuthority('resolution:write')")
 	@PostMapping("/resolution")
 	public Resolution make(@CurrentUsername String owner, @RequestBody String text) {
 		Resolution resolution = new Resolution(text, owner);
 		return this.resolutions.save(resolution);
 	}
 
+	@PreAuthorize("hasAuthority('resolution:write')")
 	@PutMapping(path="/resolution/{id}/revise")
 	@Transactional
 	public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
@@ -42,6 +47,7 @@ public class ResolutionController {
 		return read(id);
 	}
 
+	@PreAuthorize("hasAuthority('resolution:write')")
 	@PutMapping("/resolution/{id}/complete")
 	@Transactional
 	public Optional<Resolution> complete(@PathVariable("id") UUID id) {
