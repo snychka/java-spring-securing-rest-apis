3c777cf01ec459417b623b9eb22d2c057439cdd6
snychka <stefannychka@gmail.com>
2020-05-15 22:10:47 -0600

    manually integrate my most recent code

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -1,18 +1,22 @@
 package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
 public class ResolutionInitializer implements SmartInitializingSingleton {
+	@Autowired
+	private final UserRepository users;
+
 	private final ResolutionRepository resolutions;
 
-	public ResolutionInitializer(ResolutionRepository resolutions) {
+	public ResolutionInitializer(ResolutionRepository resolutions, UserRepository users) {
 		this.resolutions = resolutions;
+		this.users = users;
 	}
-
 	@Override
 	public void afterSingletonsInstantiated() {
 		UUID joshId = UUID.fromString("219168d2-1da4-4f8a-85d8-95b4377af3c1");
@@ -25,5 +29,27 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 		this.resolutions.save(new Resolution("Run for President", carolId));
 		this.resolutions.save(new Resolution("Run a Marathon", carolId));
 		this.resolutions.save(new Resolution("Run an Errand", carolId));
+
+
+		User user = new User();
+		user.setUsername("user");
+		user.setPassword("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+
+		user.grantAuthority("READ");
+		user.grantAuthority("WRITE");
+
+		this.users.save(user);
+
+		User hasread = new User();
+		hasread.setUsername("hasread");
+		hasread.setPassword("password");
+		hasread.grantAuthority("READ");
+		this.users.save(hasread);
+
+		User haswrite = new User();
+		haswrite.setUsername("haswrite");
+		haswrite.setPassword("password");
+		haswrite.grantAuthority("WRITE");
+		this.users.save(haswrite);
 	}
 }
