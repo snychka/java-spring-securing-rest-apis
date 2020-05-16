14271d257f63dcbc10c77de3c403cffb790ea3d6
snychka <stefannychka@gmail.com>
2020-05-15 22:09:29 -0600

    manually update to match master on parent

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -15,8 +15,15 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 
 	@Override
 	public void afterSingletonsInstantiated() {
-		this.resolutions.save(new Resolution("Read War and Peace", "user"));
-		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
-		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+		UUID joshId = UUID.fromString("219168d2-1da4-4f8a-85d8-95b4377af3c1");
+		UUID carolId = UUID.fromString("328167d1-2da3-5f7a-86d7-96b4376af2c0");
+
+		this.resolutions.save(new Resolution("Read War and Peace", joshId));
+		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", joshId));
+		this.resolutions.save(new Resolution("Hang Christmas Lights", joshId));
+
+		this.resolutions.save(new Resolution("Run for President", carolId));
+		this.resolutions.save(new Resolution("Run a Marathon", carolId));
+		this.resolutions.save(new Resolution("Run an Errand", carolId));
 	}
 }
