367f6527de4d24d6764fb505f0ec5bc146bc1b12
snychka <stefannychka@gmail.com>
2020-05-21 22:15:39 -0600

    sorta finish modul4
    - 'cause couldn't pass task6, tasks 7 and 8 also didn't pass
    - also, could have problems in my code for tasks 7 and 8, it's tough to
      tell

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -25,21 +25,22 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
 		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
 
-
-
 		User user = new User("user", "{bcrypt}$2a$10$3njzOWhsz20aimcpMamJhOnX9Pb4Nk3toq8OO0swIy5EPZnb1YyGe");
 		user.setFullName("User Userson");
 		user.grantAuthority("resolution:read");
+		user.grantAuthority("user:read");
 		this.users.save(user);
 
 		User hasRead = new User("hasread", "{bcrypt}$2a$10$3njzOWhsz20aimcpMamJhOnX9Pb4Nk3toq8OO0swIy5EPZnb1YyGe");
 		hasRead.setFullName("Reader Readson");
 		hasRead.grantAuthority("resolution:read");
+		hasRead.grantAuthority("user:read");
 		this.users.save(hasRead);
 
 		User hasWrite = new User("haswrite", "{bcrypt}$2a$10$3njzOWhsz20aimcpMamJhOnX9Pb4Nk3toq8OO0swIy5EPZnb1YyGe");
 		hasWrite.setFullName("Writer Writeson");
 		hasWrite.grantAuthority("resolution:write");
+		hasWrite.grantAuthority("user:read");
 		this.users.save(hasWrite);
 
 		User admin = new User("{bcrypt}$2a$10$bTu5ilpT4YILX8dOWM/05efJnoSlX4ElNnjhNopL9aPoRyUgvXAYa", "admin");
